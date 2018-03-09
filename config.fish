550c244b577c743c9c327225b830768e82b27521
chucker34 <kawakami34@gmail.com>
2018-03-09 21:32:49 +0900

    設定更新

--- config.fish
+++ config.fish
@@ -12,7 +12,6 @@ function sync_history --on-event fish_preexec
 end
 
 set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
-set -x PATH /usr/local/heroku/bin $PATH
 set -x PATH /usr/local/opt/openssl/bin $PATH
 set -x PATH /usr/local/var/nodebrew/current/bin $PATH
 set -x NODEBREW_ROOT /usr/local/var/nodebrew
@@ -20,6 +19,7 @@ set -x PATH /Users/kawakami/Library/Android/sdk/platform-tools $PATH
 set -x PATH /Users/kawakami/Library/Android/sdk/tools $PATH
 # for serverless framework
 set -x PATH ./node_modules/.bin $PATH
+set -x COMPOSE_HTTP_TIMEOUT 200
 # 反映されないから/Users/kawakami/.config/fisherman/z/functions/z.fishに直接書いた
 # set -gx Z_SCRIPT_PATH /usr/local/etc/profile.d/z.sh
 

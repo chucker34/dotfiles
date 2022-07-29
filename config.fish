152d179e4031c1e30d650be8ec26dcaad63b0345
chucker34 <kawakami34@gmail.com>
2022-07-29 12:24:44 +0900

    path変更

--- config.fish
+++ config.fish
@@ -2,6 +2,8 @@ source ~/.profile
 
 set -g theme_display_ruby no
 
+set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
+
 function fish_user_key_bindings
   bind \cr peco_select_history # Bind for prco history to Ctrl+r
 end
@@ -11,9 +13,6 @@ function sync_history --on-event fish_preexec
   history --merge
 end
 
-set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
-set -x PATH /usr/local/opt/openssl/bin $PATH
-
 # Android
 set -x PATH ~/Library/Android/sdk/platform-tools $PATH
 set -x PATH ~/Library/Android/sdk/tools $PATH
@@ -21,7 +20,7 @@ set -x PATH ~/Library/Android/sdk/tools/bin/sdkmanager $PATH
 set -x ANDROID_HOME $HOME/Library/Android/sdk
 set -x ANDROID_SDK_ROOT ~/Library/Android/sdk
 set -x ANDROID_AVD_HOME ~/.android/avd
-set -x JAVA_HOME "/Applications/Android Studio.app/Contents/jre/jdk/Contents/Home"
+set -x JAVA_HOME "/Library/Java/JavaVirtualMachines/zulu-11.jdk/Contents/Home"
 
 # nvm
 function nvm
@@ -48,3 +47,7 @@ eval (direnv hook fish)
 #rbenv
 set -x PATH $HOME/.rbenv/bin $PATH
 status --is-interactive; and source (rbenv init -|psub)
+
+# openssl
+set -x PATH /usr/local/opt/openssl@1.1/bin $PATH
+

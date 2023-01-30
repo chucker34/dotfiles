6c985b6e420ae9abc7653879abe83019406fb350
chucker34 <kawakami34@gmail.com>
2023-01-30 15:15:23 +0900

    devboxのパスを優先するようにする
    .envrcでdevboxのパスを優先するようにしてたけど、.envrcがないディレクトリでは実行されないので、devboxが起動していれば常に優先するようにした

--- config.fish
+++ config.fish
@@ -28,27 +28,30 @@ if test '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
   bass source '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
 end
 
-# rbenv
-set -x PATH $HOME/.rbenv/bin $PATH
-status --is-interactive; and source (rbenv init -|psub)
+# devbox shellが起動してない場合はPATHを通す
+if not test $DEVBOX_SHELL_ENABLED
+  # rbenv
+  set -x PATH $HOME/.rbenv/bin $PATH
+  status --is-interactive; and source (rbenv init -|psub)
 
-# nvm
-function nvm
-  bass source (brew --prefix nvm)/nvm.sh --no-use ';' nvm $argv
+  # nvm
+  function nvm
+    bass source (brew --prefix nvm)/nvm.sh --no-use ';' nvm $argv
+  end
+  set -x NVM_DIR ~/.nvm
+  nvm use default --silent
+  set -x NODE_VERSIONS ~/.nvm/versions/node
+  set -x NODE_VERSION_PREFIX v
+
+  # openssl
+  set -x PATH /usr/local/opt/openssl@1.1/bin $PATH
+
+  # Android
+  set -x PATH ~/Library/Android/sdk/platform-tools $PATH
+  set -x PATH ~/Library/Android/sdk/tools $PATH
+  set -x PATH ~/Library/Android/sdk/tools/bin/sdkmanager $PATH
+  set -x ANDROID_HOME $HOME/Library/Android/sdk
+  set -x ANDROID_SDK_ROOT ~/Library/Android/sdk
+  set -x ANDROID_AVD_HOME ~/.android/avd
+  set -x JAVA_HOME "/Library/Java/JavaVirtualMachines/zulu-11.jdk/Contents/Home"
 end
-set -x NVM_DIR ~/.nvm
-nvm use default --silent
-set -x NODE_VERSIONS ~/.nvm/versions/node
-set -x NODE_VERSION_PREFIX v
-
-# openssl
-set -x PATH /usr/local/opt/openssl@1.1/bin $PATH
-
-# Android
-set -x PATH ~/Library/Android/sdk/platform-tools $PATH
-set -x PATH ~/Library/Android/sdk/tools $PATH
-set -x PATH ~/Library/Android/sdk/tools/bin/sdkmanager $PATH
-set -x ANDROID_HOME $HOME/Library/Android/sdk
-set -x ANDROID_SDK_ROOT ~/Library/Android/sdk
-set -x ANDROID_AVD_HOME ~/.android/avd
-set -x JAVA_HOME "/Library/Java/JavaVirtualMachines/zulu-11.jdk/Contents/Home"
